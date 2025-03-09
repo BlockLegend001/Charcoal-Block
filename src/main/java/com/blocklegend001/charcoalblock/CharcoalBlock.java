a5a1fa36db1cd0a3cc8c54cfcd649d06fd2a4d6f
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 12:28:27 +0100

    Aggiunta 1.19.2-Fabric

---
+++ src/main/java/com/blocklegend001/charcoalblock/CharcoalBlock.java
@@ -0,0 +1,33 @@
+package com.blocklegend001.charcoalblock;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class CharcoalBlock implements ModInitializer {
+    public static final String MOD_ID = "charcoalblock";
+
+        public static Block CHARCOAL_BLOCK = new Block(FabricBlockSettings.
+                of(Material.STONE).
+                mapColor(MapColor.BLACK).
+                requiresTool().
+                strength(5.0F, 6.0F));
+        public static BlockItem CHARCOAL_BLOCK_ITEM = new BlockItem(CHARCOAL_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "charcoal_block"), CHARCOAL_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "charcoal_block"), CHARCOAL_BLOCK_ITEM);
+
+        FuelRegistry fuelRegistry = FuelRegistry.INSTANCE;
+        fuelRegistry.add(CHARCOAL_BLOCK, 16000);
+    }
+}
