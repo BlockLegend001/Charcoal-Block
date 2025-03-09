46c0764524a6e0053b107c54ed7e954bae1fa1e2
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 12:24:37 +0100

    Aggiunta 1.19-Forge

--- src/main/java/com/blocklegend001/charcoalblock/CharcoalBlock.java
+++ src/main/java/com/blocklegend001/charcoalblock/CharcoalBlock.java
@@ -2,16 +2,15 @@ package com.blocklegend001.charcoalblock;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -31,12 +30,11 @@ public class CharcoalBlock {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
     public static final RegistryObject<Block> CHARCOAL_BLOCK = BLOCKS.register("charcoal_block", () -> new Block(
-            BlockBehaviour.Properties.ofFullCopy(Blocks.STONE).
-                    mapColor(MapColor.COLOR_BLACK).
+            BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_BLACK).
                     requiresCorrectToolForDrops().
                     strength(5.0F, 6.0F
     )));
-    public static final RegistryObject<Item> CHARCOAL_BLOCK_ITEM = ITEMS.register("charcoal_block", () -> new BlockItem(CHARCOAL_BLOCK.get(), new Item.Properties())
+    public static final RegistryObject<Item> CHARCOAL_BLOCK_ITEM = ITEMS.register("charcoal_block", () -> new BlockItem(CHARCOAL_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS))
     {
         @Override
         public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType)
@@ -51,16 +49,9 @@ public class CharcoalBlock {
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
-        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         LOGGER.info("Mod Loaded");
-    }
-
-    public void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(new ItemStack(CHARCOAL_BLOCK_ITEM.get()));
-        }
     }
 }
