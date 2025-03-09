ab98a066fdaae87a9730abd5821c03bf28f7c4d2
BlockLegend001 <rosarioarcamone@gmail.com>
2025-03-09 12:28:50 +0100

    Aggiunta 1.20.6-Fabric

---
+++ src/main/java/com/blocklegend001/charcoalblock/client/CharcoalBlockClient.java
@@ -0,0 +1,10 @@
+package com.blocklegend001.charcoalblock.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CharcoalBlockClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
