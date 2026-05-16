c76e6443b1cf7c4eaa61363f252835f9a185c290
BlockLegend001 <rosarioarcamone@gmail.com>
2026-05-17 00:17:05 +0200

    Added 26.1.2-NeoForge

---
+++ src/main/java/com/blocklegend001/charcoalblock/datagen/providers/ModBlockTagsProvider.java
@@ -0,0 +1,21 @@
+package com.blocklegend001.charcoalblock.datagen.providers;
+
+import com.blocklegend001.charcoalblock.CharcoalBlock;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagsProvider extends BlockTagsProvider {
+
+    public ModBlockTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, String modId) {
+        super(output, lookupProvider, modId);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider lookupProvider) {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(CharcoalBlock.CHARCOAL_BLOCK.get());
+    }
+}
